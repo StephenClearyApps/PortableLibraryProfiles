fadacab0cff3ca66861fa2d51eee90baae331199
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-03 20:08:35 -0400

    Updated packages.

--- src/FrameworkProfiles/NugetTargets.cs
+++ src/FrameworkProfiles/NugetTargets.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Versioning;
 using System.Text;
-using Comparers;
-using EqualityComparers;
 
 namespace FrameworkProfiles
 {
